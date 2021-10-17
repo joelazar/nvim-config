a274ff7cfbed42500a1fddc512fe1ad1968dcf90
joelazar <jozsef.gabor.lazar@gmail.com>
2021-10-17 17:25:57 +0200

    :wrench: lualine config updated

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,36 +1,17 @@
 local M = {}
 
-local function lsp_progress()
-    local messages = vim.lsp.util.get_progress_messages()
-    if #messages == 0 then return end
-    local status = {}
-    for _, msg in pairs(messages) do
-        table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
-    end
-    local spinners = {
-        "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"
-    }
-    local ms = vim.loop.hrtime() / 1000000
-    local frame = math.floor(ms / 120) % #spinners
-    return table.concat(status, " | ") .. " " .. spinners[frame + 1]
-end
-
-vim.cmd([[autocmd User LspProgressUpdate let &ro = &ro]])
-
 M.config = {
     options = {
         theme = 'nightfox',
         section_separators = {"", ""},
         component_separators = {"", ""},
-        -- section_separators = { "", "" },
-        -- component_separators = { "", "" },
         icons_enabled = true
     },
     sections = {
         lualine_a = {"mode"},
         lualine_b = {"branch"},
         lualine_c = {{"diagnostics", sources = {"nvim_lsp"}}, "filename"},
-        lualine_x = {"filetype", lsp_progress},
+        lualine_x = {"filetype", "encoding"},
         lualine_y = {"progress"},
         lualine_z = {"location"}
     },
@@ -41,8 +22,8 @@ M.config = {
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
-    }
-    -- extensions = { "nvim-tree" },
+    },
+    extensions = {"quickfix", "toggleterm"}
 }
 
 M.setup = function()
