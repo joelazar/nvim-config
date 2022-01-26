ce4ee4b30ad06044879500b5a0f12989913a2309
joelazar <jozsef.gabor.lazar@gmail.com>
2022-01-26 21:29:21 +0100

    :alien: gitsigns config keys have changed

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -36,14 +36,16 @@ M.config = {
 	numhl = false,
 	linehl = false,
 	keymaps = {},
-	watch_index = { interval = 1000, follow_files = true },
+	watch_gitdir = { interval = 1000, follow_files = true },
 	current_line_blame = false,
 	current_line_blame_opts = { delay = 1000 },
 	sign_priority = 6,
 	update_debounce = 200,
 	status_formatter = nil, -- Use default
 	word_diff = false,
-	use_internal_diff = true, -- If luajit is present
+	diff_opts = {
+		internal = true, -- If luajit is present
+	},
 }
 
 M.setup = function()
