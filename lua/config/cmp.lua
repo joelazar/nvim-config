8475efeac4717153e7d9227f9bdb43cb5dc83664
joelazar <jozsef.gabor.lazar@gmail.com>
2022-02-01 01:03:27 +0100

    :wrench: cmp sorting algo added from TJ's config :pray:

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -86,6 +86,31 @@ M.setup = function()
 			}),
 		},
 
+		sorting = {
+			comparators = {
+				cmp.config.compare.offset,
+				cmp.config.compare.exact,
+				cmp.config.compare.score,
+
+				function(entry1, entry2)
+					local _, entry1_under = entry1.completion_item.label:find("^_+")
+					local _, entry2_under = entry2.completion_item.label:find("^_+")
+					entry1_under = entry1_under or 0
+					entry2_under = entry2_under or 0
+					if entry1_under > entry2_under then
+						return false
+					elseif entry1_under < entry2_under then
+						return true
+					end
+				end,
+
+				cmp.config.compare.kind,
+				cmp.config.compare.sort_text,
+				cmp.config.compare.length,
+				cmp.config.compare.order,
+			},
+		},
+
 		experimental = { native_menu = false, ghost_text = true },
 	})
 
@@ -96,7 +121,7 @@ M.setup = function()
 	cmp.setup.cmdline(":", {
 		sources = cmp.config.sources({
 			{ name = "path" },
-			{ name = "cmdline", max_item_count = 20, keyword_length = 3 },
+			{ name = "cmdline", keyword_length = 3 },
 		}),
 	})
 end
