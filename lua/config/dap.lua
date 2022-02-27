ecf7250b8af4ca63b7c631b4cd541f0d17d9bdc6
joelazar <jozsef.gabor.lazar@gmail.com>
2022-02-27 19:05:17 +0100

    :wrench: extend dap config

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -14,6 +14,7 @@ M.setup = function()
 		},
 	}
 
+	-- TODO - not tested
 	dap.configurations.javascript = {
 		{
 			type = "node2",
@@ -26,6 +27,7 @@ M.setup = function()
 		},
 	}
 
+	-- TODO - not working yet
 	dap.configurations.typescript = {
 		{
 			type = "node2",
@@ -36,6 +38,25 @@ M.setup = function()
 			sourceMaps = true,
 			protocol = "inspector",
 			console = "integratedTerminal",
+		},
+	}
+
+	dap.adapters.firefox = {
+		type = "executable",
+		command = "node",
+		args = { os.getenv("HOME") .. "/git/js/vscode-firefox-debug/dist/adapter.bundle.js" },
+	}
+
+	-- TODO - not working yet
+	dap.configurations.typescriptreact = {
+		{
+			name = "Debug with Firefox",
+			type = "firefox",
+			request = "launch",
+			reAttach = true,
+			url = "http://localhost:3000",
+			webRoot = "${workspaceFolder}",
+			firefoxExecutable = "/usr/bin/firefox",
 		},
 	}
 
@@ -82,6 +103,35 @@ M.setup = function()
 			mode = "test",
 			program = ".",
 			dlvToolPath = vim.fn.exepath("dlv"),
+		},
+	}
+
+	-- TODO - not tested
+	dap.configurations.python = {
+		{
+			{
+				-- The first three options are required by nvim-dap
+				type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+				request = "launch",
+				name = "Launch file",
+
+				-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+				program = "${file}", -- This configuration will launch the current file if used.
+				pythonPath = function()
+					-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+					-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+					-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+					local cwd = vim.fn.getcwd()
+					if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+						return cwd .. "/venv/bin/python"
+					elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+						return cwd .. "/.venv/bin/python"
+					else
+						return "/usr/bin/python"
+					end
+				end,
+			},
 		},
 	}
 end
