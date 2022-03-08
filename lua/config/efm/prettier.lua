ce7f54a30f2b216c0e8c8f1a935f8feb40b1da62
joelazar <jozsef.gabor.lazar@gmail.com>
2022-03-08 01:23:32 +0100

    :fire: no need for simple prettier setup

--- lua/config/efm/prettier.lua
+++
@@ -1,6 +0,0 @@
-return {
-	formatCommand = ([[
-        $([ -n "$(command -v node_modules/.bin/prettier)" ] && echo "node_modules/.bin/prettier" || echo "prettier")
-        ${--config-precedence:configPrecedence}
-    ]]):gsub("\n", ""),
-}
