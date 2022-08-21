79b19336cab98379286b35091dec4318ad1c192a
joelazar <jozsef.gabor.lazar@gmail.com>
2022-08-21 18:07:50 +0200

    :alien: update to latest go.nvim settings

--- lua/config/go.lua
+++ lua/config/go.lua
@@ -1,13 +1,10 @@
 local M = {}
 
 M.config = {
-	goimport = "gopls", -- if set to 'gopls' will use golsp format
-	gofmt = "gopls", -- if set to gopls will use golsp format
-	tag_transform = false,
 	test_dir = "",
 	comment_placeholder = "   ",
 	lsp_cfg = false, -- false: use your own lspconfig
-	lsp_on_attach = false, -- use on_attach from go.nvim
+	lsp_on_attach = nil, -- use on_attach from go.nvim
 	dap_debug = true,
 }
 
