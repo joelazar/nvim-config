b81506a8b16ad4fe66c54421cd3a872c7ceeeb15
joelazar <jozsef.gabor.lazar@gmail.com>
2023-01-02 14:12:40 +0100

    :fire: remove gitsigns codeaction from null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,7 +8,6 @@ M.config = function()
 	null_ls.setup({
 		sources = {
 			null_ls.builtins.code_actions.eslint_d,
-			null_ls.builtins.code_actions.gitsigns,
 			null_ls.builtins.code_actions.shellcheck,
 
 			null_ls.builtins.completion.spell,
