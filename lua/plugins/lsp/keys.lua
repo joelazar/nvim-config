ac07db1f1e54c5b3dcded0712914db852c80ea18
joelazar <jozsef.gabor.lazar@gmail.com>
2023-01-02 14:13:01 +0100

    :bug: fix lsp signature commands

--- lua/plugins/lsp/keys.lua
+++ lua/plugins/lsp/keys.lua
@@ -5,8 +5,10 @@ function M.setup(buffer)
 
 	local keymap = {
 		buffer = buffer,
-		["<C-k>"] = { "<cmd>Lspsaga signature_help<CR>", "Signature Help", mode = { "n", "i" } },
-		["K"] = { "<cmd>Lspsaga hover_doc<CR>", "Hover" },
+		["<C-k>"] = { "<cmd>Lspsaga hover_doc<CR>", "Signature Help", mode = { "n", "i" } },
+		["K"] = { "<cmd>Lspsaga hover_doc<CR>", "Signature Help" },
+		["gk"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help - builtin lsp" },
+
 		["[d"] = { "<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>", "Next Diagnostic" },
 		["]d"] = { "<cmd>lua vim.diagnostic.goto_next({ float = false })<cr><CR>", "Prev Diagnostic" },
 		["[e"] = {
@@ -21,7 +23,6 @@ function M.setup(buffer)
 		["gD"] = { "<cmd>Lspsaga peek_definition<CR>", "Peek definition" },
 		["gr"] = { "<cmd>Lspsaga lsp_finder<CR>", "Lspsaga finder" },
 		["gi"] = { "<cmd>Lspsaga implement<CR>", "Go to implementation" },
-		["gk"] = { "<cmd>Lspsaga signature_help<CR>", "Signature Help" },
 		["gR"] = { "<cmd>lua vim.lsp.buf.references()<CR>", "Go to references" },
 		["gT"] = { "<cmd>lua vim.lsp.buf.type_definition()<CR>", "Type definition" },
 	}
