b2e3302a0d4c6a9218b1ff0bb6fccc39b345b15a
joelazar <jozsef.gabor.lazar@gmail.com>
2023-01-08 12:17:10 +0100

    :wrench: update plugins and change mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,59 +3,60 @@ local M = {
 	cmd = { "Mason" },
 }
 
-M.tools = {
-	"actionlint",
-	"bash-language-server",
-	"black",
-	"chrome-debug-adapter",
-	"clangd",
-	"css-lsp",
-	"delve",
-	"dockerfile-language-server",
-	"eslint-lsp",
-	"eslint_d",
-	"firefox-debug-adapter",
-	"flake8",
-	"gofumpt",
-	"golangci-lint-langserver",
-	"gopls",
-	"gotestsum",
-	"html-lsp",
-	"isort",
-	"js-debug-adapter",
-	"json-lsp",
-	"lua-language-server",
-	"mypy",
-	"node-debug2-adapter",
-	"prettierd",
-	"prisma-language-server",
-	"pylint",
-	"pyright",
-	"rust-analyzer",
-	"shellcheck",
-	"shfmt",
-	"sqlfluff",
-	"stylelint-lsp",
-	"stylua",
-	"tailwindcss-language-server",
-	"texlab",
-	"typescript-language-server",
-	"yaml-language-server",
-}
-
-function M.check()
-	local mr = require("mason-registry")
-	for _, tool in ipairs(M.tools) do
-		local p = mr.get_package(tool)
-		if not p:is_installed() then
-			p:install()
-		end
-	end
-end
-
 function M.config()
 	require("mason").setup()
-	M.check()
+
+	local tools = {
+		"actionlint",
+		"bash-language-server",
+		"black",
+		"chrome-debug-adapter",
+		"clangd",
+		"css-lsp",
+		"delve",
+		"dockerfile-language-server",
+		"eslint-lsp",
+		"eslint_d",
+		"firefox-debug-adapter",
+		"flake8",
+		"gofumpt",
+		"golangci-lint-langserver",
+		"gopls",
+		"gotestsum",
+		"html-lsp",
+		"isort",
+		"js-debug-adapter",
+		"json-lsp",
+		"lua-language-server",
+		"mypy",
+		"node-debug2-adapter",
+		"prettierd",
+		"prisma-language-server",
+		"pylint",
+		"pyright",
+		"rust-analyzer",
+		"shellcheck",
+		"shfmt",
+		"sqlfluff",
+		"stylelint-lsp",
+		"stylua",
+		"tailwindcss-language-server",
+		"texlab",
+		"typescript-language-server",
+		"yaml-language-server",
+	}
+
+	local function check()
+		local mr = require("mason-registry")
+		for _, tool in ipairs(tools) do
+			local p = mr.get_package(tool)
+			if not p:is_installed() then
+				p:install()
+			end
+		end
+	end
+
+	check()
 end
 
 return M
