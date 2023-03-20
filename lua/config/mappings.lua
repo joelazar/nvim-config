c7eec51fd7bb4d0f0ba59053d8657436c5a62751
joelazar <jozsef.gabor.lazar@gmail.com>
2023-03-20 02:31:01 +0100

    :recycle: update barbar mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -4,30 +4,30 @@ local opts = { noremap = true, silent = true }
 -- Normal mode
 
 -- Move to previous/next
-map("n", "<A-Left>", ":BufferPrevious<cr>", opts)
-map("n", "<A-Right>", ":BufferNext<cr>", opts)
+map("n", "<A-Left>", "<cmd>BufferPrevious<cr>", opts)
+map("n", "<A-Right>", "<cmd>BufferNext<cr>", opts)
 
 -- Re-order to previous/next
-map("n", "<A-,>", ":BufferMovePrevious<cr>", opts)
-map("n", "<A-.>", " :BufferMoveNext<cr>", opts)
+map("n", "<A-,>", "<cmd>BufferMovePrevious<cr>", opts)
+map("n", "<A-.>", "<cmd>BufferMoveNext<cr>", opts)
 
 -- Goto buffer in position...
-map("n", "<A-1>", ":BufferGoto 1<cr>", opts)
-map("n", "<A-2>", ":BufferGoto 2<cr>", opts)
-map("n", "<A-3>", ":BufferGoto 3<cr>", opts)
-map("n", "<A-4>", ":BufferGoto 4<cr>", opts)
-map("n", "<A-5>", ":BufferGoto 5<cr>", opts)
-map("n", "<A-6>", ":BufferGoto 6<cr>", opts)
-map("n", "<A-7>", ":BufferGoto 7<cr>", opts)
-map("n", "<A-8>", ":BufferGoto 8<cr>", opts)
-map("n", "<A-9>", ":BufferGoto 9<cr>", opts)
-map("n", "<A-0>", ":BufferLast<cr>", opts)
+map("n", "<A-1>", "<cmd>BufferGoto 1<cr>", opts)
+map("n", "<A-2>", "<cmd>BufferGoto 2<cr>", opts)
+map("n", "<A-3>", "<cmd>BufferGoto 3<cr>", opts)
+map("n", "<A-4>", "<cmd>BufferGoto 4<cr>", opts)
+map("n", "<A-5>", "<cmd>BufferGoto 5<cr>", opts)
+map("n", "<A-6>", "<cmd>BufferGoto 6<cr>", opts)
+map("n", "<A-7>", "<cmd>BufferGoto 7<cr>", opts)
+map("n", "<A-8>", "<cmd>BufferGoto 8<cr>", opts)
+map("n", "<A-9>", "<cmd>BufferGoto 9<cr>", opts)
+map("n", "<A-0>", "<cmd>BufferLast<cr>", opts)
 
 -- Pin buffer
-map("n", "<A-p>", ":BufferPin<cr>", opts)
+map("n", "<A-p>", "<cmd>BufferPin<cr>", opts)
 
 -- Close buffer
-map("n", "<A-c>", ":BufferClose<cr>", opts)
+map("n", "<A-c>", "<cmd>BufferClose<cr>", opts)
 
 -- Close windows
 map("n", "Q", ":close<cr>", opts)
