e7356b7a172d57b8e401db7bff9093e52e6cb558
joelazar <jozsef.gabor.lazar@gmail.com>
2023-03-27 10:02:09 +0200

    :wrench: change chatgpt config a bit, update livegrep keymap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -163,8 +163,10 @@ return {
 			require("chatgpt").setup({
 				welcome_message = "",
 				keymaps = {
-					close = { "<C-c>", "<Esc>" },
+					close = { "<C-c>" },
+					submit = "<C-Enter>",
 					yank_last = "<C-y>",
+					yank_last_code = "<C-k>",
 					scroll_up = "<C-u>",
 					scroll_down = "<C-d>",
 					toggle_settings = "<C-o>",
