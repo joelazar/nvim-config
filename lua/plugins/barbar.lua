d640c81576070806c6fd62a559be66f6e27c91eb
joelazar <jozsef.gabor.lazar@gmail.com>
2023-03-31 10:37:42 +0200

    :alien: change barbar config to v2

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -9,7 +9,7 @@ local M = {
 M.config = function()
 	require("bufferline").setup({
 		-- Enable/disable animations
-		animation = false,
+		animation = true,
 
 		-- Enable/disable auto-hiding the tab bar when there is a single buffer
 		auto_hide = false,
@@ -17,49 +17,54 @@ M.config = function()
 		-- Enable/disable current/total tabpages indicator (top right corner)
 		tabpages = true,
 
-		-- Enable/disable close button
-		closable = false,
-
 		-- Enables/disable clickable tabs
 		--  - left-click: go to buffer
 		--  - middle-click: delete buffer
 		clickable = false,
 
-		-- Enables / disables diagnostic symbols
-		diagnostics = {
-			-- you can use a list
-			{ enabled = true, icon = "ﬀ" }, -- ERROR
-			{ enabled = false }, -- WARN
-			{ enabled = false }, -- INFO
-			{ enabled = true }, -- HINT
+		-- Disable highlighting alternate buffers
+		highlight_alternate = false,
 
-			-- OR `vim.diagnostic.severity`
-			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
-			[vim.diagnostic.severity.WARN] = { enabled = false },
-			[vim.diagnostic.severity.INFO] = { enabled = false },
-			[vim.diagnostic.severity.HINT] = { enabled = true },
-		},
+		-- Disable highlighting file icons in inactive buffers
+		highlight_inactive_file_icons = false,
 
 		-- Enable highlighting visible buffers
 		highlight_visible = true,
 
-		-- Enable/disable icons
-		-- if set to 'numbers', will show buffer index in the tabline
-		-- if set to 'both', will show buffer index and icons in the tabline
-		icons = true,
+		icons = {
+			-- Configure the base icons on the bufferline.
+			buffer_index = false,
+			buffer_number = false,
+			button = "",
+			-- Enables / disables diagnostic symbols
+			diagnostics = {
+				[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
+				[vim.diagnostic.severity.WARN] = { enabled = false },
+				[vim.diagnostic.severity.INFO] = { enabled = false },
+				[vim.diagnostic.severity.HINT] = { enabled = true },
+			},
+			filetype = {
+				-- Sets the icon's highlight group.
+				-- If false, will use nvim-web-devicons colors
+				custom_colors = true,
 
-		-- If set, the icon color will follow its corresponding buffer
-		-- highlight group. By default, the Buffer*Icon group is linked to the
-		-- Buffer* group (see Highlighting below). Otherwise, it will take its
-		-- default value as defined by devicons.
-		icon_custom_colors = true,
+				-- Requires `nvim-web-devicons` if `true`
+				enabled = true,
+			},
+			separator = { left = "▎", right = "" },
 
-		-- Configure icons on the bufferline.
-		icon_separator_active = "▎",
-		icon_separator_inactive = "▎",
-		icon_close_tab = "",
-		icon_close_tab_modified = "●",
-		icon_pinned = "車",
+			-- Configure the icons on the bufferline when modified or pinned.
+			-- Supports all the base icon options.
+			modified = { button = "●" },
+			pinned = { button = "車" },
+
+			-- Configure the icons on the bufferline based on the visibility of a buffer.
+			-- Supports all the base icon options, plus `modified` and `pinned`.
+			alternate = { filetype = { enabled = false } },
+			current = { buffer_index = false },
+			-- inactive = { button = "×" },
+			visible = { modified = { buffer_number = false } },
+		},
 
 		-- If true, new buffers will be inserted at the start/end of the list.
 		-- Default is to insert after current buffer.
@@ -82,7 +87,7 @@ M.config = function()
 		semantic_letters = true,
 
 		-- New buffer letters are assigned in this order. This order is
-		-- optimal for the qwerty keyboard layout but might need adjustement
+		-- optimal for the qwerty keyboard layout but might need adjustment
 		-- for other layouts.
 		letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
 
