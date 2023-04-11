d5ffad2c22bbd22dded51ef7a21361a8090d865c
joelazar <jozsef.gabor.lazar@gmail.com>
2023-04-11 22:39:13 +0200

    :fire: no need for diagnostics in buffer titles

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -38,10 +38,10 @@ M.config = function()
 			button = "",
 			-- Enables / disables diagnostic symbols
 			diagnostics = {
-				[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
+				[vim.diagnostic.severity.ERROR] = { enabled = false, icon = "ﬀ" },
 				[vim.diagnostic.severity.WARN] = { enabled = false },
 				[vim.diagnostic.severity.INFO] = { enabled = false },
-				[vim.diagnostic.severity.HINT] = { enabled = true },
+				[vim.diagnostic.severity.HINT] = { enabled = false },
 			},
 			filetype = {
 				-- Sets the icon's highlight group.
