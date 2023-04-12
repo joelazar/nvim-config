a916fac66fea9661569c5d591f6cb66f993675d8
joelazar <jozsef.gabor.lazar@gmail.com>
2023-04-12 13:20:42 +0200

    :lipstick: position which-key menu to the center

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -43,7 +43,7 @@ M.config = function()
 				height = { min = 4, max = 25 }, -- min and max height of the columns
 				width = { min = 20, max = 50 }, -- min and max width of the columns
 				spacing = 3, -- spacing between columns
-				align = "left",
+				align = "center",
 			},
 			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
 			hidden = {
