3e6db988044b9019b86b2800e32b22f28d2a0870
joelazar <jozsef.gabor.lazar@gmail.com>
2023-04-21 16:22:20 +0200

    :fire: disable scope_incremental

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -65,9 +65,9 @@ M.config = function()
 			enable = true,
 			keymaps = {
 				init_selection = "<CR>",
-				scope_incremental = "<CR>",
 				node_incremental = "<TAB>",
 				node_decremental = "<S-TAB>",
+				scope_incremental = false,
 			},
 		},
 		indent = { enable = true },
