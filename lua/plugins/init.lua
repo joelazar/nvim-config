80bf436986632d65c7b17b1d2816125d496489b9
joelazar <jozsef.gabor.lazar@gmail.com>
2023-04-28 08:09:29 +0200

    :wrench: update chatgpt plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -161,21 +161,31 @@ return {
 		"jackMort/ChatGPT.nvim",
 		config = function()
 			require("chatgpt").setup({
-				welcome_message = "",
-				keymaps = {
-					close = { "<C-c>" },
-					submit = "<C-Enter>",
-					yank_last = "<C-y>",
-					yank_last_code = "<C-k>",
-					scroll_up = "<C-u>",
-					scroll_down = "<C-d>",
-					toggle_settings = "<C-o>",
-					new_session = "<C-n>",
-					cycle_windows = "<Tab>",
-					-- in the Sessions pane
-					select_session = "<Space>",
-					rename_session = "r",
-					delete_session = "d",
+				edit_with_instructions = {
+					diff = false,
+					keymaps = {
+						accept = "<C-y>",
+						toggle_diff = "<C-d>",
+						toggle_settings = "<C-o>",
+						cycle_windows = "<Tab>",
+						use_output_as_input = "<C-i>",
+					},
+				},
+				chat = {
+					welcome_message = "",
+					keymaps = {
+						close = { "<C-c>" },
+						yank_last = "<C-y>",
+						yank_last_code = "<C-k>",
+						scroll_up = "<C-u>",
+						scroll_down = "<C-d>",
+						toggle_settings = "<C-o>",
+						new_session = "<C-n>",
+						cycle_windows = "<Tab>",
+						select_session = { "<Space>", "o", "<cr>" },
+						rename_session = "r",
+						delete_session = "d",
+					},
 				},
 				-- openai_params = {
 				-- 	model = "gpt-3.5-turbo",
