7809c173aa976ed8cdab4929bae2c7f7c9d75328
joelazar <jozsef.gabor.lazar@gmail.com>
2023-05-01 09:00:58 +0200

    :fire: simplify dap setup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,7 +11,6 @@ local M = {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"theHamsta/nvim-dap-virtual-text",
-		"mxsdev/nvim-dap-vscode-js",
 		"mfussenegger/nvim-dap-python",
 	},
 }
@@ -20,88 +19,23 @@ M.config = function()
 	local dap = require("dap")
 	local dapui = require("dapui")
 	local dap_vt = require("nvim-dap-virtual-text")
-	local dap_vscode_js = require("dap-vscode-js")
 	local dap_python = require("dap-python")
 
-	dap_vt.setup({
-		enabled = true, -- enable this plugin (the default)
-		enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-		highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-		highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-		show_stop_reason = true, -- show stop reason when stopped for exceptions
-		commented = false, -- prefix virtual text with comment string
-		only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-		all_references = false, -- show virtual text on all all references of the variable (not only definitions)
-		filter_references_pattern = "<module", -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
-		-- Experimental Features:
-		virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
-		all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-		virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-		virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-	})
+	dap_vt.setup({})
 
 	-- DAP UI
-	dapui.setup({
-		icons = { expanded = "▾", collapsed = "▸" },
-		mappings = {
-			-- Use a table to apply multiple mappings
-			expand = { "<CR>", "<2-LeftMouse>" },
-			open = "o",
-			remove = "d",
-			edit = "e",
-			repl = "r",
-			toggle = "t",
-		},
-		-- Expand lines larger than the window
-		-- Requires >= 0.7
-		expand_lines = vim.fn.has("nvim-0.7"),
-		-- Layouts define sections of the screen to place windows.
-		-- The position can be "left", "right", "top" or "bottom".
-		-- The size specifies the height/width depending on position. It can be an Int
-		-- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-		-- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-		-- Elements are the elements shown in the layout (in order).
-		-- Layouts are opened in order so that earlier layouts take priority in window sizing.
-		layouts = {
-			{
-				elements = {
-					-- Elements can be strings or table with id and size keys.
-					{ id = "scopes", size = 0.25 },
-					"breakpoints",
-					"stacks",
-					"watches",
-				},
-				size = 40, -- 40 columns
-				position = "left",
-			},
-			{
-				elements = {
-					"repl",
-					"console",
-				},
-				size = 0.25, -- 25% of total lines
-				position = "bottom",
-			},
-		},
-		floating = {
-			max_height = nil, -- These can be integers or a float between 0 and 1.
-			max_width = nil, -- Floats will be treated as percentage of your screen.
-			border = "rounded", -- Border style. Can be "single", "double" or "rounded"
-			mappings = {
-				close = { "q", "<Esc>" },
-			},
-		},
-		windows = { indent = 1 },
-		render = {
-			max_type_length = nil, -- Can be integer or nil.
-		},
-	})
+	dapui.setup()
 
-	vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
-	vim.fn.sign_define("DapBreakpointRejected", { text = "🟦", texthl = "", linehl = "", numhl = "" })
-	vim.fn.sign_define("DapStopped", { text = "⭐️", texthl = "", linehl = "", numhl = "" })
+	vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "", linehl = "", numhl = "" })
+	vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
+	vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "", linehl = "", numhl = "" })
+	vim.fn.sign_define(
+		"DapStopped",
+		{ text = " ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "DapStoppedLine" }
+	)
+	vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "", linehl = "", numhl = "" })
 
-	dap.set_log_level("TRACE")
+	-- dap.set_log_level("TRACE")
 
 	-- Automatically open UI
 	dap.listeners.after.event_initialized["dapui_config"] = function()
@@ -114,23 +48,14 @@ M.config = function()
 		dapui.close()
 	end
 
-	-- Enable virtual text
-	vim.g.dap_virtual_text = true
-
 	-- Keybindings
 	local opts = { noremap = true, silent = true }
-	vim.api.nvim_set_keymap("n", "<F9>", "<CMD>lua require('dap').toggle_breakpoint()<CR>", opts)
 	vim.api.nvim_set_keymap("n", "<F5>", "<CMD>lua require('dap').continue()<CR>", opts)
+	vim.api.nvim_set_keymap("n", "<S-F5>", "<CMD>lua require('dap').terminate()<CR>", opts)
+	vim.api.nvim_set_keymap("n", "<F9>", "<CMD>lua require('dap').toggle_breakpoint()<CR>", opts)
+	vim.api.nvim_set_keymap("n", "<F10>", "<CMD>lua require('dap').step_over()<CR>", opts)
 	vim.api.nvim_set_keymap("n", "<F11>", "<CMD>lua require('dap').step_into()<CR>", opts)
 	vim.api.nvim_set_keymap("n", "<S-F11>", "<CMD>lua require('dap').step_out()<CR>", opts)
-	vim.api.nvim_set_keymap("n", "<F10>", "<CMD>lua require('dap').step_over()<CR>", opts)
-	vim.api.nvim_set_keymap("n", "<S-F5>", "<CMD>lua require('dap').terminate()<CR>", opts)
-
-	dap_vscode_js.setup({
-		node_path = "/usr/bin/node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-		debugger_path = { vim.fn.stdpath("data") .. "/mason/packages/vscode-js-debug/" }, -- Path to vscode-js-debug installation. TODO - does it work?
-		adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-	})
 
 	dap.adapters.firefox = {
 		type = "executable",
@@ -149,6 +74,17 @@ M.config = function()
 		command = "node",
 		args = { vim.fn.stdpath("data") .. "/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js" },
 	}
+
+	-- TODO: not working
+	-- dap.adapters["pwa-node"] = {
+	-- 	type = "server",
+	-- 	host = "127.0.0.1",
+	-- 	port = "9229",
+	-- 	executable = {
+	-- 		command = "js-debug-adapter",
+	-- 		args = { "9229" },
+	-- 	},
+	-- }
 
 	dap.configurations.typescript = {
 		-- Working configs 🎉
@@ -210,11 +146,35 @@ M.config = function()
 			cwd = "${workspaceFolder}",
 		},
 		{
-			name = "Nope - Node - Attach with pwa-node",
 			type = "pwa-node",
-			skipFiles = { "<node_internals>/**" },
+			request = "launch",
+			name = "Nope - Node - Launch with pwa-node",
+			program = "${file}",
+			cwd = "${workspaceFolder}",
+			port = 9229,
+		},
+		{
+			type = "pwa-node",
 			request = "attach",
+			name = "Nope - Node - Attach with pwa-node",
 			processId = require("dap.utils").pick_process,
+			-- cwd = "${workspaceFolder}/src",
+			-- port = 9229,
+		},
+		{
+			type = "pwa-node",
+			request = "launch",
+			name = "Nope - Node - Debug Jest Tests with pwa-node",
+			-- trace = true, -- include debugger info
+			runtimeExecutable = "node",
+			runtimeArgs = {
+				"./node_modules/jest/bin/jest.js",
+				"--runInBand",
+			},
+			rootPath = "${workspaceFolder}",
+			cwd = "${workspaceFolder}",
+			console = "integratedTerminal",
+			internalConsoleOptions = "neverOpen",
 		},
 	}
 
