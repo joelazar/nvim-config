4739dd03ea153bd330a1d231cbe4e081c01a5bc8
joelazar <jozsef.gabor.lazar@gmail.com>
2023-05-15 12:41:49 +0200

    :sparkles: add back ruff linter

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -19,7 +19,7 @@ M.config = function()
 			null_ls.builtins.diagnostics.mypy,
 			-- null_ls.builtins.diagnostics.flake8,
 			-- null_ls.builtins.diagnostics.pylint,
-			-- null_ls.builtins.diagnostics.ruff,
+			null_ls.builtins.diagnostics.ruff,
 			null_ls.builtins.diagnostics.shellcheck,
 			null_ls.builtins.diagnostics.sqlfluff.with({
 				extra_args = { "--dialect", "postgres" },
