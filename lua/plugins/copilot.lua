59d5e7eed2a10cc46f3dcb814ac773626bc0174e
joelazar <jozsef.gabor.lazar@gmail.com>
2023-05-21 18:31:31 +0200

    :recycle: update copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,5 +5,28 @@ return {
 	opts = {
 		suggestion = { enabled = false },
 		panel = { enabled = false },
+		filetypes = {
+			yaml = true,
+			markdown = true,
+			gitcommit = true,
+			gitrebase = true,
+			hgcommit = false,
+			help = false,
+			svn = false,
+			cvs = false,
+			["."] = false,
+			fish = function()
+				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%..*") then
+					return false
+				end
+				return true
+			end,
+			sh = function()
+				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%..*") then
+					return false
+				end
+				return true
+			end,
+		},
 	},
 }
