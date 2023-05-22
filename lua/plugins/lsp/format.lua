17bbd1f59a804c0ee03bb11a53df52bd9c089f45
joelazar <jozsef.gabor.lazar@gmail.com>
2023-05-22 11:03:13 +0200

    :bug: fix attaching null-ls format clients

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -47,17 +47,15 @@ function M.on_attach(client, buf)
 		return
 	end
 
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
-			buffer = buf,
-			callback = function()
-				if M.autoformat then
-					M.format()
-				end
-			end,
-		})
-	end
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
+		buffer = buf,
+		callback = function()
+			if M.autoformat then
+				M.format()
+			end
+		end,
+	})
 end
 
 return M
