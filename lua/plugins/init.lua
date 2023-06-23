ca962b8be15a745958f28815135588893784e085
joelazar <jozsef.gabor.lazar@gmail.com>
2023-06-23 15:41:36 +0200

    :arrow_up: update plugins + update flash.nvim config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -111,13 +111,23 @@ return {
 					-- default options: exact mode, multi window, all directions, with a backdrop
 					require("flash").jump()
 				end,
+				desc = "Flash",
 			},
 			{
 				"S",
-				mode = { "o", "x" },
+				mode = { "n", "o", "x" },
 				function()
 					require("flash").treesitter()
 				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
 			},
 		},
 	},
