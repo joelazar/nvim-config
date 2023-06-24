61d831070f8364956d685551cf45cc9651c192d8
joelazar <jozsef.gabor.lazar@gmail.com>
2023-06-24 17:44:53 +0200

    :wrench: add telescope flash integration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,11 +12,31 @@ local M = {
 
 M.config = function()
 	local telescope = require("telescope")
+	local function flash(prompt_bufnr)
+		require("flash").jump({
+			pattern = "^",
+			highlight = { label = { after = { 0, 0 } } },
+			search = {
+				mode = "search",
+				exclude = {
+					function(win)
+						return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+					end,
+				},
+			},
+			action = function(match)
+				local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+				picker:set_selection(match.pos[1] - 1)
+			end,
+		})
+	end
 	local config = {
 		defaults = {
 			path_display = { truncate = 3 },
 			mappings = {
+				n = { s = flash },
 				i = {
+					["<c-s>"] = flash,
 					-- ["<esc>"] = require("telescope.actions").close,
 					["<S-Up>"] = require("telescope.actions").preview_scrolling_up,
 					["<S-Down>"] = require("telescope.actions").preview_scrolling_down,
