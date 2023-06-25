5600c9da0cc4f8bde72dd22a2955cba973d11516
joelazar <jozsef.gabor.lazar@gmail.com>
2023-06-25 10:14:14 +0200

    :recycle: refactor autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,37 +1,39 @@
-local vimrc_group = vim.api.nvim_create_augroup("vimrc", { clear = true })
+local function augroup(name)
+	return vim.api.nvim_create_augroup("vimrc_" .. name, { clear = true })
+end
 
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 	desc = "Check if we need to reload the file when it changed",
 	command = "checktime",
-	group = vimrc_group,
+	group = augroup("checktime"),
 })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
 	desc = "Hide cursorline in insert mode",
 	pattern = "*",
 	command = "set nocul",
-	group = vimrc_group,
+	group = augroup("cursorline_hide_insert"),
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
 	desc = "Show cursorline when leaving insert mode",
 	pattern = "*",
 	command = "set cul",
-	group = vimrc_group,
+	group = augroup("cursorline_show_insert"),
 })
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter", "CmdlineLeave" }, {
 	desc = "Show cursorline only in active window",
 	pattern = "*",
 	command = "set cursorline",
-	group = vimrc_group,
+	group = augroup("cursorline_show_active"),
 })
 
 vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave", "CmdlineEnter" }, {
 	desc = "Hide cursorline only in inactive window",
 	pattern = "*",
 	command = "set nocursorline",
-	group = vimrc_group,
+	group = augroup("cursorline_hide_inactive"),
 })
 
 vim.api.nvim_create_autocmd("BufReadPost", {
@@ -39,37 +41,40 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
 		vim.cmd([[silent! normal! g`"]])
 	end,
-	group = vimrc_group,
+	group = augroup("cursorline_hide_inactive"),
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-	desc = "Don't auto commenting new lines",
-	pattern = "*",
-	command = "set fo-=c fo-=r fo-=o",
-	group = vimrc_group,
+vim.api.nvim_create_autocmd("BufReadPost", {
+	desc = "Go to last loc when opening a buffer",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+	group = augroup("last_loc"),
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
 	desc = "Don't show any numbers inside terminals",
 	pattern = "term://*",
 	command = "setlocal signcolumn=no nonumber norelativenumber | setfiletype terminal",
-	group = vimrc_group,
+	group = augroup("terminal_no_numbers"),
 })
-
-local highlight_group = vim.api.nvim_create_augroup("Highlight", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight on yank",
 	callback = function()
 		vim.highlight.on_yank({ higrou = "IncSearch", timeout = 400 })
 	end,
-	group = highlight_group,
+	group = augroup("highlight_yank"),
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	desc = "Create directories when needed, when saving a file",
-	group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
 	command = [[call mkdir(expand('<afile>:p:h'), 'p')]],
+	group = augroup("auto_create_dir"),
 })
 
 vim.api.nvim_create_autocmd("TermClose", {
@@ -80,7 +85,7 @@ vim.api.nvim_create_autocmd("TermClose", {
 			vim.api.nvim_input("<CR>")
 		end
 	end,
-	group = vimrc_group,
+	group = augroup("terminal_close_at_exit"),
 })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
@@ -88,7 +93,7 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 	callback = function()
 		vim.cmd("tabdo wincmd =")
 	end,
-	group = vimrc_group,
+	group = augroup("resize_splits"),
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -98,6 +103,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		"TelescopePrompt",
 		"chatgpt",
 		"checkhealth",
+		"dap-repl",
 		"help",
 		"lspinfo",
 		"man",
@@ -114,13 +120,5 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		vim.bo[event.buf].buflisted = false
 		vim.keymap.set("n", "q", "<cmd>close!<cr>", { buffer = event.buf, silent = true })
 	end,
-	group = vimrc_group,
-})
-
--- dap-repl buffer should not be listed
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "dap-repl",
-	callback = function(args)
-		vim.api.nvim_buf_set_option(args.buf, "buflisted", false)
-	end,
+	group = augroup("close_with_q"),
 })
