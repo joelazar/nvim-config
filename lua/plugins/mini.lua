c4675c588c2215c200d7155606cda30e5babda51
joelazar <jozsef.gabor.lazar@gmail.com>
2023-06-27 22:48:56 +0200

    :wrench: map surrounding plugin to gz* (trying it out for now)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -97,6 +97,15 @@ return {
 					["["] = { output = { left = "[", right = "]" } },
 					["]"] = { output = { left = "[", right = "]" } },
 				},
+				mappings = {
+					add = "gza", -- Add surrounding in Normal and Visual modes
+					delete = "gzd", -- Delete surrounding
+					find = "gzf", -- Find surrounding (to the right)
+					find_left = "gzF", -- Find surrounding (to the left)
+					highlight = "gzh", -- Highlight surrounding
+					replace = "gzr", -- Replace surrounding
+					update_n_lines = "gzn", -- Update `n_lines`
+				},
 			})
 		end,
 	},
