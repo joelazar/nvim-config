6c5ea149c277294997fd2ac4985c11ed99b17f25
joelazar <jozsef.gabor.lazar@gmail.com>
2023-07-13 00:19:32 +0200

    :heavy_plus_sign: add venv selector plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -168,4 +168,11 @@ return {
 	},
 
 	{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+		config = true,
+		event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	},
 }
