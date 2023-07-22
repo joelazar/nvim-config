5e644a4af27c67c38ac59cfa361ffbc0679a9bea
joelazar <jozsef.gabor.lazar@gmail.com>
2023-07-22 21:16:52 +0200

    :wrench: configure flatten.nvim to do not flatten nvim instance in the same kitty instance

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,7 +20,11 @@ return {
 
 	{
 		"willothy/flatten.nvim",
-		config = true,
+		config = {
+			one_per = {
+				kitty = false,
+			},
+		},
 		-- Ensure that it runs first to minimize delay when opening file from terminal
 		lazy = false,
 		priority = 1001,
