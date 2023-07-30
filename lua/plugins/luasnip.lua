6d468955253bf1240d2268a975622aa59ccc71c3
joelazar <jozsef.gabor.lazar@gmail.com>
2023-07-30 13:12:13 +0200

    :arrow_up: update to luasnip v2

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,7 @@
 local M = {
 	"L3MON4D3/LuaSnip",
+	version = "2.*",
+	build = "make install_jsregexp",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 		config = function()
@@ -19,12 +21,16 @@ function M.config()
 	vim.cmd([[
     " press <Tab> to expand or jump in a snippet. These can also be mapped separately
     " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
     " -1 for jumping backwards.
     inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
     snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
     snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+
+    " For changing choices in choiceNodes (not strictly necessary for a basic setup).
+    imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+    smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
   ]])
 end
 
