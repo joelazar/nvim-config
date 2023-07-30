ecd6d2c4ae90a7768a767c7426733accc4a5e1b1
joelazar <jozsef.gabor.lazar@gmail.com>
2023-07-30 13:11:59 +0200

    :wrench: move keymaps to plugin config

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -95,24 +95,6 @@ vim.cmd([[
 map("n", "<C-q>", "<cmd>call QuickFixToggle()<cr>", silent)
 map("n", "<C-`>", "<cmd>ToggleTerm<cr>", silent)
 
--- resizing splits
--- these keymaps will also accept a range,
--- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
--- moving between splits
-vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
--- swapping buffers between windows
-vim.keymap.set("n", "<C-A-h>", require("smart-splits").swap_buf_left)
-vim.keymap.set("n", "<C-A-j>", require("smart-splits").swap_buf_down)
-vim.keymap.set("n", "<C-A-k>", require("smart-splits").swap_buf_up)
-vim.keymap.set("n", "<C-A-l>", require("smart-splits").swap_buf_right)
-
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 vim.keymap.set("n", "n", "'Nn'[v:searchforward]", { expr = true })
 vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true })
