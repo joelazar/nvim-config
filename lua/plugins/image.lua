5fa01e756c2a818c37f02f19295a33e6b7dfa574
joelazar <jozsef.gabor.lazar@gmail.com>
2023-08-12 15:55:34 +0200

    :heavy_plus_sign: add image.nvim

---
+++ lua/plugins/image.lua
@@ -0,0 +1,30 @@
+return {
+	"3rd/image.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		-- + luarocks install magick
+	},
+	ft = { "markdown" },
+	opts = {
+		backend = "kitty",
+		integrations = {
+			markdown = {
+				enabled = true,
+				sizing_strategy = "auto",
+				download_remote_images = true,
+				clear_in_insert_mode = false,
+			},
+			neorg = {
+				enabled = false,
+			},
+		},
+		max_width = nil,
+		max_height = nil,
+		max_width_window_percentage = nil,
+		max_height_window_percentage = 50,
+		kitty_method = "normal",
+		kitty_tmux_write_delay = 10,
+		window_overlap_clear_enabled = false,
+		window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+	},
+}
