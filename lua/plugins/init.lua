dc487e895126ea52eb9d2223c4f9b592dbc57f8c
joelazar <jozsef.gabor.lazar@gmail.com>
2023-08-24 09:48:25 +0200

    :heavy_plus_sign: add autolist plugin for markdown editing

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,4 +199,25 @@ return {
 			require("wildfire").setup()
 		end,
 	},
+
+	{
+		"gaoDean/autolist.nvim",
+		ft = { "markdown" },
+		config = function()
+			require("autolist").setup()
+
+			vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+			vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+			-- vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+			vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+			vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+			-- vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+			vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+
+			vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+			vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+			vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+			vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+		end,
+	},
 }
