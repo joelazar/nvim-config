d5c2be6ad8198c73649bb19fb5c69b6fe76ecd7d
joelazar <jozsef.gabor.lazar@gmail.com>
2023-08-27 23:21:02 +0200

    :heavy_plus_sign: add hardtime

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,4 +199,26 @@ return {
 			require("wildfire").setup()
 		end,
 	},
+
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {
+			restricted_keys = {
+				["h"] = { "n", "x" },
+				["j"] = { "n", "x" },
+				["k"] = { "n", "x" },
+				["l"] = { "n", "x" },
+				["-"] = { "n", "x" },
+				["+"] = { "n", "x" },
+				["gj"] = { "n", "x" },
+				["gk"] = { "n", "x" },
+				["<CR>"] = { "n", "x" },
+				["<C-M>"] = { "n", "x" },
+				["<C-N>"] = { "n", "x" },
+				["<C-P>"] = {},
+			},
+		},
+		event = { "BufReadPost", "BufNewFile" },
+	},
 }
