e0f277874cc96cafbc0a2aace947a7ebb81042e4
joelazar <jozsef.gabor.lazar@gmail.com>
2023-08-31 11:57:44 +0200

    :wrench: disable flash search by default

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,13 @@
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		modes = {
+			search = {
+				enabled = false,
+			},
+		},
+	},
   -- stylua: ignore
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
