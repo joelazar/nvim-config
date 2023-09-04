10c22d5971aec14a0d61a91b4a113e021de3ccc2
joelazar <jozsef.gabor.lazar@gmail.com>
2023-09-04 22:37:59 +0200

    :heavy_plus_sign: add wtf.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -221,4 +221,30 @@ return {
 		},
 		event = { "BufReadPost", "BufNewFile" },
 	},
+
+	{
+		"piersolenski/wtf.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {},
+		keys = {
+			{
+				"gw",
+				mode = { "n", "x" },
+				function()
+					require("wtf").ai()
+				end,
+				desc = "Debug diagnostic with AI",
+			},
+			{
+				mode = { "n" },
+				"gW",
+				function()
+					require("wtf").search()
+				end,
+				desc = "Search diagnostic with Google",
+			},
+		},
+	},
 }
