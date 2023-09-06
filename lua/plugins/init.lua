a14c3db80d54ab8f980cecf4322b3199a677ee18
joelazar <jozsef.gabor.lazar@gmail.com>
2023-09-06 08:19:59 +0200

    :heavy_plus_sign: add nvim-devdocs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -247,4 +247,25 @@ return {
 			},
 		},
 	},
+
+	{
+		"luckasRanarison/nvim-devdocs",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+		cmd = {
+			"DevdocsFetch",
+			"DevdocsInstall",
+			"DevdocsUninstall",
+			"DevdocsOpen",
+			"DevdocsOpenFloat",
+			"DevdocsOpenCurrent",
+			"DevdocsOpenCurrentFloat",
+			"DevdocsUpdate",
+			"DevdocsUpdateAll",
+		},
+	},
 }
