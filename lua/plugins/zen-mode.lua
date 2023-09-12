d65aeda0e4c39f135af78e35c5f24a486062de0a
joelazar <jozsef.gabor.lazar@gmail.com>
2023-09-12 10:11:17 +0200

    :bug: disable hlargs at zenmode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -5,10 +5,16 @@ return {
 		plugins = {
 			twilight = { enabled = true },
 			gitsigns = { enabled = true },
-			kitty = {
-				enabled = true,
-				font = "+4",
-			},
+			-- kitty = {
+			-- 	enabled = true,
+			-- 	font = "+4",
+			-- },
 		},
+		on_open = function(win)
+			require("hlargs").disable()
+		end,
+		on_close = function()
+			require("hlargs").enable()
+		end,
 	},
 }
