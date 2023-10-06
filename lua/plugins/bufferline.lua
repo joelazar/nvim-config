ed73b9a8fc89dcfa4983ed584ba3957c91f0ae3e
joelazar <jozsef.gabor.lazar@gmail.com>
2023-10-06 13:04:27 +0200

    :art: fix format for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,9 +1,9 @@
 return {
 	"akinsho/bufferline.nvim",
-	-- dependencies = {
-	-- 	"nvim-tree/nvim-web-devicons",
-	-- 	"echasnovski/mini.bufremove",
-	-- },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"echasnovski/mini.bufremove",
+	},
 	event = "VeryLazy",
 	config = function()
 		require("bufferline").setup({
@@ -21,14 +21,12 @@ return {
 			highlights = require("catppuccin.groups.integrations.bufferline").get(),
 		})
 	end,
-	-- stylua: ignore
 	keys = {
-	  { "<A-Left>",  "<cmd>BufferLineCyclePrev<cr>",  mode = "n", desc = "Move to previous buffer" },
-	  { "<A-Right>", "<cmd>BufferLineCycleNext<cr>",  mode = "n", desc = "Move to next buffer" },
-	  { "<A-Tab>",   "<cmd>BufferLineCycleNext<cr>",  mode = "n", desc = "Move to next buffer" },
-	  { "<A-,>",     "<cmd>BufferLineMovePrev<cr>",   mode = "n", desc = "Re-order to previous buffer" },
-	  { "<A-.>",     "<cmd>BufferLineMoveNext<cr>",   mode = "n", desc = "Re-order to next buffer" },
-	  { "<A-p>",     "<cmd>BufferLineTogglePin<cr>",  mode = "n", desc = "Pin buffer" },
-	}
-,
+		{ "<A-Left>", "<cmd>BufferLineCyclePrev<cr>", mode = "n", desc = "Move to previous buffer" },
+		{ "<A-Right>", "<cmd>BufferLineCycleNext<cr>", mode = "n", desc = "Move to next buffer" },
+		{ "<A-Tab>", "<cmd>BufferLineCycleNext<cr>", mode = "n", desc = "Move to next buffer" },
+		{ "<A-,>", "<cmd>BufferLineMovePrev<cr>", mode = "n", desc = "Re-order to previous buffer" },
+		{ "<A-.>", "<cmd>BufferLineMoveNext<cr>", mode = "n", desc = "Re-order to next buffer" },
+		{ "<A-p>", "<cmd>BufferLineTogglePin<cr>", mode = "n", desc = "Pin buffer" },
+	},
 }
