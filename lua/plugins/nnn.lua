b255622134fe5668efbe41bb05dbc33732c2d159
joelazar <jozsef.gabor.lazar@gmail.com>
2023-10-06 13:04:38 +0200

    :wrench: do not lazyload nnn

--- lua/plugins/nnn.lua
+++ lua/plugins/nnn.lua
@@ -1,6 +1,7 @@
 local M = {
 	"luukvbaal/nnn.nvim",
 	cmd = { "NnnPicker", "NnnExplorer" },
+	lazy = false,
 }
 
 M.config = function()
@@ -33,7 +34,7 @@ M.config = function()
 				"gitcommit",
 			},
 		},
-		auto_close = false, -- close tabpage/nvim when nnn is last window
+		auto_close = true, -- close tabpage/nvim when nnn is last window
 		replace_netrw = "picker",
 		mappings = {
 			{ "<C-t>", require("nnn").builtin.open_in_tab }, -- open file(s) in tab
