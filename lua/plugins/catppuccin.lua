b455516e383d01043f5ac7a9da4fa662440835a1
joelazar <jozsef.gabor.lazar@gmail.com>
2023-10-18 14:24:34 +0200

    :lipstick: update catppuccin config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,44 +3,66 @@ return {
 	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		integrations = {
-			barbar = true,
-			cmp = true,
-			dap = {
-				enabled = true,
-				enable_ui = true, -- enable nvim-dap-ui
-			},
-			flash = true,
-			gitsigns = true,
-			indent_blankline = { enabled = true },
-			lsp_trouble = true,
-			lualine = true,
-			markdown = true,
-			mason = true,
-			mini = true,
-			native_lsp = {
-				enabled = true,
-				underlines = {
-					errors = { "undercurl" },
-					hints = { "undercurl" },
-					warnings = { "undercurl" },
-					information = { "undercurl" },
-				},
-			},
-			neotest = true,
-			noice = true,
-			notify = true,
-			octo = true,
-			overseer = true,
-			semantic_tokens = true,
-			telescope = true,
-			treesitter = true,
-			ts_rainbow2 = true,
-			which_key = true,
-		},
-	},
 	config = function()
+		require("catppuccin").setup({
+			dim_inactive = {
+				enabled = true, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.45, -- percentage of the shade to apply to the inactive window
+			},
+			integrations = {
+				cmp = true,
+				dap = {
+					enabled = true,
+					enable_ui = true, -- enable nvim-dap-ui
+				},
+				flash = true,
+				gitsigns = true,
+				indent_blankline = {
+					enabled = true,
+					scope_color = "lavender", -- catppuccin color (eg. `lavender`) Default: text
+					colored_indent_levels = false,
+				},
+				lsp_trouble = true,
+				markdown = true,
+				mason = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				neotest = true,
+				noice = true,
+				notify = true,
+				octo = true,
+				overseer = true,
+				semantic_tokens = true,
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
+				ts_rainbow2 = true,
+				ufo = true,
+				which_key = true,
+			},
+		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
