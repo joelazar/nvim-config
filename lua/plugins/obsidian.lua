b3d506173f27c8355cc30188dc170f7c9f4d4b60
joelazar <jozsef.gabor.lazar@gmail.com>
2023-11-19 20:19:56 +0100

    :wrench: obsidian config update

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -11,6 +11,7 @@ return {
 		"ObsidianLinkNew",
 		"ObsidianNew",
 		"ObsidianOpen",
+		"ObsidianPasteImg",
 		"ObsidianQuickSwitch",
 		"ObsidianRename",
 		"ObsidianSearch",
@@ -34,16 +35,31 @@ return {
 			},
 
 			completion = {
+				-- If using nvim-cmp, otherwise set to false
 				nvim_cmp = true,
+				-- Trigger completion at 2 chars
 				min_chars = 2,
 				-- Where to put new notes created from completion. Valid options are
 				--  * "current_dir" - put new notes in same directory as the current buffer.
 				--  * "notes_subdir" - put new notes in the default notes subdirectory.
 				new_notes_location = "current_dir",
 
-				-- Whether to add the output of the node_id_func to new notes in autocompletion.
+				-- Control how wiki links are completed with these options:
+
+				-- Whether to add the note ID during completion.
 				-- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-				prepend_note_id = false,
+				-- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
+				prepend_note_id = true,
+
+				-- Whether to add the note path during completion.
+				-- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
+				-- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
+				prepend_note_path = false,
+
+				-- Whether to only use paths during completion.
+				-- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
+				-- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
+				use_path_only = false,
 			},
 
 			-- Optional, key mappings.
