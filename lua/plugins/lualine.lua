2c3d2ac04913770a68b352364f727d33012c83e5
joelazar <jozsef.gabor.lazar@gmail.com>
2024-01-03 20:35:20 +0100

    :heavy_plus_sign: add copilot-lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,7 @@ local M = {
 	"nvim-lualine/lualine.nvim",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+		"AndreM222/copilot-lualine",
 	},
 	event = "VeryLazy",
 }
@@ -24,13 +25,20 @@ M.config = function()
 
 	require("lualine").setup({
 		options = {
+			icons_enabled = true,
 			theme = "catppuccin",
 			globalstatus = true,
 			disabled_filetypes = { statusline = { "starter" } },
+			always_divide_middle = true,
 		},
 		sections = {
 			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_b = {
+				"branch",
+
+				"diff",
+				"diagnostics",
+			},
 			lualine_c = {
 				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
 				{
@@ -39,7 +47,26 @@ M.config = function()
 					symbols = { modified = "", readonly = "  readonly", unnamed = "" },
 				},
 			},
-			lualine_x = { "searchcount", "encoding", "fileformat", "filetype" },
+			lualine_x = {
+				{
+					"copilot",
+					symbols = {
+						status = {
+							icons = {
+								enabled = " ",
+								disabled = " ",
+								warning = " ",
+								unknown = " ",
+							},
+						},
+					},
+					show_colors = false,
+				},
+				"searchcount",
+				"encoding",
+				"fileformat",
+				"filetype",
+			},
 			lualine_y = { "progress" },
 			lualine_z = {
 				{ wordcount, cond = is_textfile },
