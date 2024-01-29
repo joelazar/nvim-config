66d37dbe3eb768116050bac221441367350b0ef2
joelazar <jozsef.gabor.lazar@gmail.com>
2024-01-29 21:53:36 +0100

    :wrench: add mapping for replace selection

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -90,3 +90,22 @@ local function smart_dd()
 end
 
 vim.keymap.set("n", "dd", smart_dd, { noremap = true, expr = true })
+
+-- map C-r to replace selection in visual mode
+
+-- handle unpack deprecation
+table.unpack = table.unpack or unpack
+
+local function get_visual()
+	local _, ls, cs = table.unpack(vim.fn.getpos("v"))
+	local _, le, ce = table.unpack(vim.fn.getpos("."))
+	return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
+end
+
+vim.keymap.set("v", "<C-r>", function()
+	local pattern = table.concat(get_visual())
+	-- escape regex and line endings
+	pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
+	-- send substitute command to vim command line
+	vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//g<Left><Left>")
+end)
