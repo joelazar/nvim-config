f5681414e516914b15fda0bceac08ab39e195b78
joelazar <jozsef.gabor.lazar@gmail.com>
2024-01-31 10:22:34 +0100

    :wrench: update kitty-scrollback to map yank to \y

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -6,5 +6,10 @@ return {
 	event = { "User KittyScrollbackLaunch" },
 	config = function()
 		require("kitty-scrollback").setup()
+		vim.keymap.set({ "v" }, "\\Y", "<Plug>(KsbVisualYankLine)", {})
+		vim.keymap.set({ "v" }, "\\y", "<Plug>(KsbVisualYank)", {})
+		vim.keymap.set({ "n" }, "\\Y", "<Plug>(KsbNormalYankEnd)", {})
+		vim.keymap.set({ "n" }, "\\y", "<Plug>(KsbNormalYank)", {})
+		vim.keymap.set({ "n" }, "\\yy", "<Plug>(KsbYankLine)", {})
 	end,
 }
