23a28ec750d581c6d79fcc755e4123d53734e0b2
joelazar <jozsef.gabor.lazar@gmail.com>
2024-03-11 09:44:36 +0100

    :alien: update copilot-chat config to v2

--- lua/plugins/copilot_chat.lua
+++ lua/plugins/copilot_chat.lua
@@ -1,45 +1,50 @@
 return {
 	"CopilotC-Nvim/CopilotChat.nvim",
 	opts = {
-		show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
 		debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-		disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
-		language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
-		-- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-		-- temperature = 0.1,
 	},
-	build = ":UpdateRemotePlugins",
-	event = "VeryLazy",
+	-- sudo luarocks install --lua-version 5.1 tiktoken_core
+	dependencies = {
+		{ "zbirenbaum/copilot.lua" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	branch = "canary",
+	cmd = {
+		"CopilotChatClose",
+		"CopilotChatDebugInfo",
+		"CopilotChatOpen",
+		"CopilotChatReset",
+		"CopilotChatToggle",
+	},
 	keys = {
-		{ "<leader>ccb", "<cmd>CopilotChatBuffer<cr>", desc = "CopilotChat - Chat with current buffer" },
-		{ "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-		{ "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-		{
-			"<leader>ccT",
-			"<cmd>CopilotChatVsplitToggle<cr>",
-			desc = "CopilotChat - Toggle Vsplit", -- Toggle vertical split
-		},
-		{
-			"<leader>ccv",
-			":CopilotChatVisual",
-			mode = "x",
-			desc = "CopilotChat - Open in vertical split",
-		},
-		{
-			"<leader>ccx",
-			":CopilotChatInPlace<cr>",
-			mode = "x",
-			desc = "CopilotChat - Run in-place code",
-		},
-		{
-			"<leader>ccf",
-			"<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
-			desc = "CopilotChat - Fix diagnostic",
-		},
+		{ "<leader>cco", "<cmd>CopilotChatOpen<cr>", desc = "CopilotChat - Open" },
+		{ "<leader>ccc", "<cmd>CopilotChatClose<cr>", desc = "CopilotChat - Close" },
+		{ "<leader>cct", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
 		{
 			"<leader>ccr",
-			"<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
-			desc = "CopilotChat - Reset chat history and clear buffer",
+			"<cmd>CopilotChatReset<cr>",
+			desc = "CopilotChat - Reset",
+		},
+		{
+			"<leader>ccD",
+			"<cmd>CopilotChatDebugInfo<cr>",
+			desc = "CopilotChat - Debug info",
+		},
+		{
+			"<leader>cch",
+			function()
+				local actions = require("CopilotChat.actions")
+				require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+			end,
+			desc = "CopilotChat - Help actions",
+		},
+		{
+			"<leader>ccp",
+			function()
+				local actions = require("CopilotChat.actions")
+				require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+			end,
+			desc = "CopilotChat - Prompt actions",
 		},
 	},
 }
