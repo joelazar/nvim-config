8d1c6a46367cc9141af59a89510baa5c09f9605a
joelazar <jozsef.gabor.lazar@gmail.com>
2024-03-20 13:09:19 +0100

    :wrench: update neotest to v5

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
+		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
 		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
 		"nvim-neotest/neotest-go",
 		"nvim-neotest/neotest-python",
 		"haydenmeade/neotest-jest",
