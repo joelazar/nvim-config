9792305b7d02958b02e609d6b199eec9ebebf702
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-01 12:06:09 +0200

    :coffin: turn off kitty plugins for now - mainly using ghostty at this point

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -1,6 +1,6 @@
 return {
 	"mikesmithgh/kitty-scrollback.nvim",
-	enabled = true,
+	enabled = false,
 	lazy = true,
 	cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
 	event = { "User KittyScrollbackLaunch" },
