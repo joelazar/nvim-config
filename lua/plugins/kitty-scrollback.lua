4ae1b1c09213fb4869962acc37676cf01dc015b0
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-02 11:38:01 +0200

    Revert ":coffin: turn off kitty plugins for now - mainly using ghostty at this point"
    This reverts commit 9792305b7d02958b02e609d6b199eec9ebebf702.

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -1,6 +1,6 @@
 return {
 	"mikesmithgh/kitty-scrollback.nvim",
-	enabled = false,
+	enabled = true,
 	lazy = true,
 	cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
 	event = { "User KittyScrollbackLaunch" },
