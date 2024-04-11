9376be724ab2257ab660cd80e77ff0f36a36d3b9
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-11 15:11:52 +0200

    :wrench: increase max_tokens for chatgpt.nvim

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -56,7 +56,7 @@ return {
 				model = "gpt-4-1106-preview",
 				frequency_penalty = 0,
 				presence_penalty = 0,
-				max_tokens = 300,
+				max_tokens = 1000,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
