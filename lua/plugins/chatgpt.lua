75f3555fc16f4153f6833da77e40ac8bfc51af24
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-17 10:45:07 +0200

    :alien: update to latest gpt-4-turbo

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -53,7 +53,7 @@ return {
 				},
 			},
 			openai_params = {
-				model = "gpt-4-1106-preview",
+				model = "gpt-4-turbo",
 				frequency_penalty = 0,
 				presence_penalty = 0,
 				max_tokens = 1000,
@@ -62,7 +62,7 @@ return {
 				n = 1,
 			},
 			openai_edit_params = {
-				model = "gpt-4-1106-preview",
+				model = "gpt-4-turbo",
 				frequency_penalty = 0,
 				presence_penalty = 0,
 				temperature = 0,
