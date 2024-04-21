631c076dac60642e53d0e8395d5cab9cdf83554f
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-21 22:08:40 +0200

    :wrench: update sniprun config

--- lua/plugins/sniprun.lua
+++ lua/plugins/sniprun.lua
@@ -7,35 +7,20 @@ local M = {
 M.config = function()
 	require("sniprun").setup({
 		selected_interpreters = {}, --# use those instead of the default for the current filetype
-		repl_enable = { "javascript", "typescript" }, --# enable REPL-like behavior for the given interpreters
+		repl_enable = { "javascript", "typescript", "python" }, --# enable REPL-like behavior for the given interpreters
 		repl_disable = {}, --# disable REPL-like behavior for the given interpreters
-
-		interpreter_options = {
-			--# interpreter-specific options, see docs / :SnipInfo <name>
-
-			--# use the interpreter name as key
-			GFM_original = {
-				use_on_filetypes = { "markdown.pandoc" }, --# the 'use_on_filetypes' configuration key is
-				--# available for every interpreter
-			},
-			Python3_original = {
-				error_truncate = "auto", --# Truncate runtime errors 'long', 'short' or 'auto'
-				--# the hint is available for every interpreter
-				--# but may not be always respected
-			},
-		},
 
 		--# you can combo different display modes as desired and with the 'Ok' or 'Err' suffix
 		--# to filter only sucessful runs (or errored-out runs respectively)
 		display = {
-			-- "Classic", --# display results in the command-line  area
-			-- "VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
+			-- "Classic", 								 --# display results in the command-line area
+			"VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
 
 			-- "VirtualText",             --# display results as virtual text
 			-- "TempFloatingWindow",      --# display results in a floating window
 			-- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText[Ok/Err]
 			"Terminal", --# display results in a vertical split
-			-- "TerminalWithCode", --# display results and code history in a vertical split
+			-- "TerminalWithCode", 				--# display results and code history in a vertical split
 			-- "NvimNotify",              --# display with the nvim-notify plugin
 			-- "Api"                      --# return output to a programming interface
 		},
@@ -46,8 +31,9 @@ M.config = function()
 			terminal_scrollback = vim.o.scrollback, --# change terminal display scrollback lines
 			terminal_line_number = false, --# whether show line number in terminal window
 			terminal_signcolumn = false, --# whether show signcolumn in terminal window
-			terminal_persistence = true, --# always keep the terminal open (true) or close it at every occasion (false)
-			terminal_width = 45, --# change the terminal display option width
+			terminal_position = "vertical", --# or "horizontal", to open as horizontal split instead of vertical split
+			terminal_width = 45, --# change the terminal display option width (if vertical)
+			terminal_height = 20, --# change the terminal display option height (if horizontal)
 			notification_timeout = 5, --# timeout for nvim_notify output
 		},
 
