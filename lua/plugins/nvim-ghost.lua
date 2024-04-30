bfb86c90b210d162577b53628dac976a41888dea
joelazar <jozsef.gabor.lazar@gmail.com>
2024-04-30 23:57:19 +0200

    :bulb: add comment about workaround for nvim-ghost

--- lua/plugins/nvim-ghost.lua
+++ lua/plugins/nvim-ghost.lua
@@ -1,8 +1,14 @@
 return {
 	"subnut/nvim-ghost.nvim",
 	lazy = false,
+	-- until this issue (https://github.com/subnut/nvim-ghost.nvim/issues/59) is resolved
+	-- we have to install the requirements manually
+	-- in the plugin directory:
+	-- python -m pip install -r requirements.txt
 	config = function()
 		vim.g.nvim_ghost_super_quiet = 1
+		vim.g.nvim_ghost_use_script = 1
+		vim.g.nvim_ghost_python_executable = "/opt/homebrew/bin/python3"
 		vim.cmd([[
 				augroup nvim_ghost_user_autocommands
 					au User *github.com,*stackoverflow.com,*reddit.com setfiletype markdown
