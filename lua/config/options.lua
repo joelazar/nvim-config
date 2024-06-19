f9e232c21ee5aa72b0804ef5427595564ab23b79
joelazar <jozsef.gabor.lazar@gmail.com>
2024-06-19 16:24:02 +0200

    :arrow_up: update plugins + use basedpyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LSP Server to use for Python.
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_ruff = "ruff_lsp"
