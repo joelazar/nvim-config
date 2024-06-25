651a168429ab5990ed283a8c9e05bddd4649439e
joelazar <jozsef.gabor.lazar@gmail.com>
2024-06-25 16:05:32 +0200

    :arrow_up: update plugins + update new major version of lazy.nvim + disable rocks support for now

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,6 +8,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
 	defaults = { lazy = true, version = false },
 	install = { colorscheme = { "catppuccin" } },
+	rocks = { enabled = false },
 	performance = {
 		rtp = {
 			disabled_plugins = {
