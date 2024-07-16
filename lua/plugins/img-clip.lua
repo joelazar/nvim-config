81e4f5a87c1386f6e0e6ce0258298f8b058d764a
joelazar <jozsef.gabor.lazar@gmail.com>
2024-07-16 10:32:42 +0200

    :wrench: set default dir for uploaded assets

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -1,6 +1,10 @@
 return {
 	"HakonHarnes/img-clip.nvim",
-	opts = {},
+	opts = {
+		default = {
+			dir_path = "_assets",
+		},
+	},
 	cmd = "PasteImage",
 	keys = {
 		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image" },
