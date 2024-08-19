4a64a74a755fb8e25d44fd80a90636fe55ab1624
joelazar <jozsef.gabor.lazar@gmail.com>
2024-08-19 08:04:37 +0200

    :bug: fix typo

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,7 +30,8 @@ local M = {
 				{
 					"<S-Tab>",
 					function()
-						return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
+						return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>"
+							or "<S-Tab>"
 					end,
 					expr = true,
 					silent = true,
