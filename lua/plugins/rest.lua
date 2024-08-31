3ab1453c0812c70f0a873e4fe6661fe9f1624683
joelazar <jozsef.gabor.lazar@gmail.com>
2024-08-31 19:19:26 +0200

    :construction: update rest.nvim config

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,16 +1,5 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-		branch = "go-away-python",
-		config = true,
-	},
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		dependencies = { "luarocks.nvim", "j-hui/fidget.nvim" },
-		config = function()
-			require("rest_nvim").setup({ env_file = ".envrc" })
-		end,
-	},
+	"rest-nvim/rest.nvim",
+	ft = "http",
+	dependencies = { "j-hui/fidget.nvim" },
 }
