1299f2f4f7062e5c40509c50e90fcbab953c6d0f
joelazar <jozsef.gabor.lazar@gmail.com>
2024-10-03 13:15:11 +0200

    :alien: update highlight-undo config

--- lua/plugins/highlight-undo.lua
+++ lua/plugins/highlight-undo.lua
@@ -1,12 +1,5 @@
 return {
 	"tzachar/highlight-undo.nvim",
-	opts = {
-		hlgroup = "BufferCurrentCHANGED",
-		duration = 500,
-		keymaps = {
-			{ "n", "u", "undo", {} },
-			{ "n", "<C-r>", "redo", {} },
-		},
-	},
+	opts = {},
 	event = "VeryLazy",
 }
