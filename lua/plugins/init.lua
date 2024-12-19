9b4f9fa0bc5c31b8ad11c0a1b026dd39ec2ec513
joelazar <jozsef.gabor.lazar@gmail.com>
2024-12-19 12:33:00 +0100

    :sparkles: config fzf-lua and kittyscrollback plugin a bit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -100,4 +100,18 @@ return {
       },
     },
   },
+
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      oldfiles = {
+        include_current_session = true,
+      },
+      previewers = {
+        builtin = {
+          syntax_limit_b = 1024 * 100, -- 100KB
+        },
+      },
+    },
+  },
 }
