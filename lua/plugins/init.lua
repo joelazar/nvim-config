aae44b09685d3c86c64b752a10e38fd9be8499d3
joelazar <jozsef.gabor.lazar@gmail.com>
2025-03-04 11:43:53 +0100

    :heavy_plus_sign: add two more sources to blink

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,7 +70,19 @@ return {
 
   {
     "saghen/blink.cmp",
-    dependencies = { "hrsh7th/cmp-calc", "saghen/blink.compat", "Kaiser-Yang/blink-cmp-avante" },
+    dependencies = {
+      "hrsh7th/cmp-calc",
+      "saghen/blink.compat",
+      "Kaiser-Yang/blink-cmp-avante",
+      {
+        "Kaiser-Yang/blink-cmp-git",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+      {
+        "Kaiser-Yang/blink-cmp-dictionary",
+        dependencies = { "nvim-lua/plenary.nvim" },
+      },
+    },
     opts = {
       keymap = {
         ["<Tab>"] = { LazyVim.cmp.map({ "snippet_forward" }), "select_next", "fallback" },
@@ -81,15 +93,26 @@ return {
       },
       completion = { list = { selection = { preselect = false, auto_insert = false } } },
       sources = {
-        default = { "avante", "calc" },
+        default = { "avante", "git", "dictionary", "calc" },
         compat = { "calc" },
         providers = {
           calc = {
             kind = "calc",
           },
+          git = {
+            module = "blink-cmp-git",
+            name = "Git",
+            opts = {},
+          },
           avante = {
             module = "blink-cmp-avante",
             name = "Avante",
+            opts = {},
+          },
+          dictionary = {
+            module = "blink-cmp-dictionary",
+            name = "Dict",
+            min_keyword_length = 3,
             opts = {},
           },
         },
