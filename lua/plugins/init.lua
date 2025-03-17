2ba8bd5a9d22a248fc3835142f09bf1deed244ec
joelazar <jozsef.gabor.lazar@gmail.com>
2025-03-17 11:01:10 +0100

    :sparkles: add wordcount to lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -169,4 +169,28 @@ return {
     end,
     cmd = "BlameToggle",
   },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local function is_textfile()
+        local filetype = vim.bo.filetype
+        return filetype == "markdown"
+          or filetype == "asciidoc"
+          or filetype == "pandoc"
+          or filetype == "tex"
+          or filetype == "text"
+      end
+
+      local function wordcount()
+        local wc = vim.fn.wordcount()
+        local visual_words = wc.visual_words or wc.words
+        local word_string = visual_words == 1 and " word" or " words"
+        return tostring(visual_words) .. word_string
+      end
+
+      table.insert(opts.sections.lualine_z, { wordcount, cond = is_textfile })
+    end,
+  },
 }
