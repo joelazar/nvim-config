9daf18e5a394922ec92f611b53eb92ca639201e3
joelazar <jozsef.gabor.lazar@gmail.com>
2025-07-06 10:18:40 +0200

    :sparkles: Update Obsidian plugin config and markdownlint settings
    - Disable MD007 and MD010 rules in markdownlint config - Update plugin versions in lazy-lock.json - Replace Obsidian mappings with callbacks
    for checkbox toggle - Comment out old mappings in obsidian.lua for reference

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -43,28 +43,13 @@ return {
         blink = true,
       },
 
-      mappings = {
-        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ["gf"] = {
-          action = function()
-            return require("obsidian").util.gf_passthrough()
-          end,
-          opts = { noremap = false, expr = true, buffer = true },
-        },
-        -- Toggle check-boxes.
-        ["<C-c>"] = {
-          action = function()
-            return require("obsidian").util.toggle_checkbox()
-          end,
-          opts = { buffer = true },
-        },
-        -- Smart action depending on context, either follow link or toggle checkbox.
-        ["<cr>"] = {
-          action = function()
-            return require("obsidian").util.smart_action()
-          end,
-          opts = { buffer = true, expr = true },
-        },
+      callbacks = {
+        enter_note = function(_, note)
+          vim.keymap.set("n", "<C-c>", "<cmd>Obsidian toggle_checkbox<cr>", {
+            buffer = note.bufnr,
+            desc = "Toggle checkbox",
+          })
+        end,
       },
 
       -- Where to put new notes. Valid options are
