ec9573a736902bc2ca5b7375d4c7d1bbe946df0a
joelazar <jozsef.gabor.lazar@gmail.com>
2025-08-13 10:44:59 +0200

    :bug: disable obsidian ui

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -241,7 +241,7 @@ return {
       -- Optional, configure additional syntax highlighting / extmarks.
       -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
       ui = {
-        enable = true, -- set to false to disable all additional syntax features
+        enable = false, -- set to false to disable all additional syntax features
         ignore_conceal_warn = false, -- set to true to disable conceallevel specific warning
         update_debounce = 200, -- update delay after a text change (in milliseconds)
         max_file_length = 5000, -- disable UI features for files with more than this many lines
