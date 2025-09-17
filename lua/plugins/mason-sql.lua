a6308034a4ee8ec87954faa3fbc51d34597b74a4
joelazar <jozsef.gabor.lazar@gmail.com>
2025-09-17 15:56:41 +0200

    :arrow_up: update to LazyVim v15.0.0

--- lua/plugins/mason-sql.lua
+++ lua/plugins/mason-sql.lua
@@ -1,6 +1,7 @@
 -- Mason SQL tools configuration
 -- Ensures SQL linting and formatting tools are installed
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   opts = { ensure_installed = { "sqlfluff" } },
 }
+
