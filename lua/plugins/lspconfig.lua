7609cc8e53cd4168c5dbbac4ebc893f0089f65e7
joelazar <jozsef.gabor.lazar@gmail.com>
2025-10-06 11:19:20 +0200

    :wrench: configure gopls settings
    Disable staticcheck and enable experimental postfix completions.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,13 +4,20 @@ return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable mappings
+    -- disable keymaps
     keys[#keys + 1] = { "<a-p>", false }
     keys[#keys + 1] = { "<a-n>", false }
 
-    -- add Go server
+    -- configure Go LSP servers
     opts.servers = opts.servers or {}
+    opts.servers.gopls = vim.tbl_deep_extend("force", opts.servers.gopls or {}, {
+      settings = {
+        gopls = {
+          staticcheck = false,
+          experimentalPostfixCompletions = true,
+        },
+      },
+    })
     opts.servers.golangci_lint_ls = {}
   end,
 }
-
