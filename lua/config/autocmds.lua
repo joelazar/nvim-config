39c5c26605d505cc15d08b09ac2f767b07a4eb6e
joelazar <jozsef.gabor.lazar@gmail.com>
2025-10-15 19:46:41 +0200

    :wrench: work around LSP detachment issue with octo.nvim buffers
    Stops LSP clients for buffers with non-file URI schemes to prevent lingering connections when using octo.nvim.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -32,3 +32,21 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.cmd("LspStop copilot")
   end,
 })
+
+-- NOTE: Workaround for octo.nvim not detaching LSP properly
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup("lsp_detach_octo"),
+  callback = function(args)
+    local bufnr = args.buf
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+    -- Check if buffer URI has non-file scheme (e.g., octo://)
+    if bufname:match("^%w+://") and not bufname:match("^file://") then
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client then
+        -- Kill the client entirely
+        vim.lsp.stop_client(client.id)
+      end
+    end
+  end,
+})
