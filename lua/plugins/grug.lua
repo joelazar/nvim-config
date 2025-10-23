fb6e6a13b71bd31c6bf195650fe8779a84514651
joelazar <jozsef.gabor.lazar@gmail.com>
2025-10-23 09:55:48 +0200

    :wrench: disable edgy animations

---
+++ lua/plugins/grug.lua
@@ -0,0 +1,20 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = false,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      mode = { "n", "x" },
+      desc = "Search and Replace",
+    },
+  },
+}
