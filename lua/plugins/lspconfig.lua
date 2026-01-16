1f82efd00017b7df5e9970f08ce30c11cae10503
joelazar <jozsef.gabor.lazar@gmail.com>
2026-01-16 14:26:35 +0100

    :bug: preserve LazyVim LSP keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,31 +4,33 @@ return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
     opts.servers = opts.servers or {}
-    opts.servers["*"] = {
-      keys = {
-        -- disable keymaps
-        { "<a-p>", false },
-        { "<a-n>", false },
-        -- map cd to LSP rename using inc-rename
-        {
-          "cd",
-          function()
-            local inc_rename = require("inc_rename")
-            return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
-          end,
-          expr = true,
-          desc = "Rename (inc-rename.nvim)",
-          has = "rename",
-        },
-        -- map <leader>ca to code actions
-        {
-          "<leader>ca",
-          vim.lsp.buf.code_action,
-          desc = "Code Action",
-          has = "codeAction",
-        },
+    opts.servers["*"] = opts.servers["*"] or {}
+    opts.servers["*"].keys = opts.servers["*"].keys or {}
+
+    vim.list_extend(opts.servers["*"].keys, {
+      -- disable keymaps
+      { "<a-p>", false },
+      { "<a-n>", false },
+      -- map cd to LSP rename using inc-rename
+      {
+        "cd",
+        function()
+          local inc_rename = require("inc_rename")
+          return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+        end,
+        expr = true,
+        desc = "Rename (inc-rename.nvim)",
+        has = "rename",
       },
-    }
+      -- map <leader>ca to code actions
+      {
+        "<leader>ca",
+        vim.lsp.buf.code_action,
+        desc = "Code Action",
+        mode = { "n", "x" },
+        has = "codeAction",
+      },
+    })
 
     -- configure Go LSP servers
     opts.servers.gopls = vim.tbl_deep_extend("force", opts.servers.gopls or {}, {
