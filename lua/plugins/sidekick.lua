09d90d8632fd011fc69f1abefc6cb5251e6b88a6
joelazar <jozsef.gabor.lazar@gmail.com>
2026-03-17 10:35:53 +0100

    :wrench: configure sidekick.nvim window and keybindings

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -2,6 +2,11 @@ return {
   "folke/sidekick.nvim",
   opts = {
     cli = {
+      win = {
+        split = {
+          width = 0.4,
+        },
+      },
       -- mux = {
       --   backend = "tmux",
       --   enabled = true,
@@ -18,9 +23,26 @@ return {
     {
       "<D-r>",
       function()
-        require("sidekick.cli").toggle()
+        require("sidekick.cli").toggle("pi")
       end,
       desc = "Toggle AI window",
+      mode = { "n", "t", "i", "x" },
+    },
+    {
+      "<D-e>",
+      function()
+        local win = vim.api.nvim_get_current_win()
+        local width = vim.api.nvim_win_get_width(win)
+        local total = vim.o.columns
+        if width > total * 0.8 then
+          -- Restore to 40%
+          vim.api.nvim_win_set_width(win, math.floor(total * 0.4))
+        else
+          -- Maximize
+          vim.api.nvim_win_set_width(win, math.floor(total * 0.95))
+        end
+      end,
+      desc = "Toggle maximize window",
       mode = { "n", "t", "i", "x" },
     },
   },
