e913af013a4efacf589e70cbc7bfb98eaa9cc436
joelazar <jozsef.gabor.lazar@gmail.com>
2026-04-14 12:48:14 +0200

    :arrow_up: update plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@
 -- Additional language parsers configuration
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   optional = true,
   opts = { ensure_installed = { "sql", "gotmpl", "comment", "css", "latex" } },
 }
