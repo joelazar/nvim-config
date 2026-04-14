cd0d4355904dec9f4c4749de067bad1372a544ed
joelazar <jozsef.gabor.lazar@gmail.com>
2026-04-14 17:21:02 +0200

    :wrench: add dotenv filetype support

---
+++ filetype.lua
@@ -0,0 +1,8 @@
+vim.filetype.add({
+  pattern = {
+    ["%.env%.[%w_.-]+"] = "dotenv",
+  },
+})
+
+-- Use bash treesitter parser for dotenv files (syntax highlighting)
+vim.treesitter.language.register("bash", "dotenv")
