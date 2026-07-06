5062ed3e2fb387eea70997b9603b0f76b549990f
joelazar <jozsef.gabor.lazar@gmail.com>
2026-07-06 09:04:28 +0200

    :wrench: disable unused language providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,3 +30,9 @@ vim.g.copilot_nes = true
 
 -- Global border for all floating windows
 vim.o.winborder = "rounded"
+
+-- Disable unused language providers (no remote/rplugin dependencies)
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
