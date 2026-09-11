4a0dd663e177f6541c836a143f752875b16fa482
joelazar <me@joelazar.dev>
2026-09-11 17:55:31 +0200

    :fire: drop blame.nvim

--- lua/plugins/blame.lua
+++
@@ -1,9 +0,0 @@
--- Git blame integration
--- Shows git blame information in the current buffer
-return {
-  "FabijanZulj/blame.nvim",
-  config = function()
-    require("blame").setup()
-  end,
-  cmd = "BlameToggle",
-}
