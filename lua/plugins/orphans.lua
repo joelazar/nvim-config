32bded67ad610ad276bb888a281add57fd1b4334
joelazar <me@joelazar.dev>
2026-09-13 15:53:39 +0200

    :fire: drop orphans.nvim

--- lua/plugins/orphans.lua
+++
@@ -1,9 +0,0 @@
--- Orphans plugin configuration
--- Helps to find orphaned files in your project
-return {
-  "ZWindL/orphans.nvim",
-  config = function()
-    require("orphans").setup({})
-  end,
-  cmd = { "Orphans" },
-}
